f76c06457827f8633ecf127511bbac3842afc654
jenniferlsanchezh <167924327+jenniferlsanchezh@users.noreply.github.com>
2024-10-11 20:25:03 -0500

    Actualizar views.py

--- mundial_2026/apps/jugadores/views.py
+++ mundial_2026/apps/jugadores/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
-def jugadores_home(request):
-    return render(request,'jugadores/jugadores_home.html')
+def jugadores_view(request):
+    return render(request, 'jugadores.html')
